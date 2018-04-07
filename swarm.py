985f9c0884b5e6d5c5a02a06513c425143b928c8
Andrey <salexs95@yandex.ru>
2018-04-07 22:46:34 +0300

    add connect swarm

--- swarm.py
+++ swarm.py
@@ -9,16 +9,24 @@ class Swarm(object):
         pass
 
     @staticmethod
-    def init_swarm(ssh_client, , eth_adapter=DEFAULT_ADAPTER):
+    def init_swarm(ssh_client, eth_adapter=DEFAULT_ADAPTER):
         join_command = "docker swarm init --advertise-addr=%s"
         _, stdout, stderr = ssh_client.exec_command(join_command)
         token = Swarm.get_token_from_out(stdout.read())
-        pass
+        stdout.flush()
+        stderr.flush()
+        return token
+
+    @staticmethod
+    def connect_to_swarm(ssh_client, token):
+        _, _, stderr = ssh_client.exec_command(token)
+        stderr.read()
+        stderr.flush()
 
     @staticmethod
     def get_token_from_out(encoded_out):
         data = decoded_out.decode()
         sentences = data.split("\n")
-        tocken = sentences[4]
-        print(token)
-        pass
+        token = sentences[4].lstrip()
+        return token
+
